c5ba373929cc54eb596be8a49831a5a5c95e2444
Nuttpoang <todaycomgoft@gmail.com>
2016-10-01 01:09:55 +0700

    1/10/59

--- app/src/main/java/com/example/a/lannav1/CP4_30.java
+++ app/src/main/java/com/example/a/lannav1/CP4_30.java
@@ -40,7 +40,7 @@ public class CP4_30 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_cp4_20, container, false);
+        View v = inflater.inflate(R.layout.fragment_cp4_30, container, false);
 
         //restart
         newButton = (Button) v.findViewById(R.id.btrestart);
